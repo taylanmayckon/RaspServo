4d482ee2e10a25afdffe1e450dff5dff47bc9166
Taylan <eusoutaylann@gmail.com>
2025-02-11 04:53:36 -0300

    Add: PWM no GP 22 com 50Hz de freq. e teste básico

--- main.c
+++ main.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/pwm.h"
+
+#define SERVO_PIN 22 // Pino do servo
+
+uint slice; // Definindo a variávle slice no escopo global
+uint wrap = 20000; // definindo o WRAP do PWM
+uint di = 125; // Divisor inteiro 
+
+void config_servo_pwm(){
+    // Equação para a f_pwm:
+    // f_pwm = f_clk/((di+(df/16))*(wrap+1))
+    // Se fizer com di=125 e wrap=20000, a frequência bate com os 50Hz solicitados
+
+    gpio_set_function(SERVO_PIN, GPIO_FUNC_PWM); // Habilitando o pino como pwm
+    slice = pwm_gpio_to_slice_num(SERVO_PIN); // Obtendo o canal (slice) do PWM 
+    pwm_set_clkdiv(slice, di); // Definindo o divisor de clock do PWM
+    pwm_set_wrap(slice, wrap); // Definindo o valor de wrap (contador do PWM)
+    pwm_set_gpio_level(SERVO_PIN, 0.12*20000); // Iniciando com 0% de Duty Cycle
+    pwm_set_enabled(slice, true); // Habilitando o pwm no slice correspondente
+}
 
 
-int main()
-{
+int main(){
     stdio_init_all();
 
+    config_servo_pwm();
+
     while (true) {
-        printf("Hello, world!\n");
         sleep_ms(1000);
     }
 }
